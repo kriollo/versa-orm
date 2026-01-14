bc9c998b7bffa551ca580a4c1c4d1fa86ff8a085
kr107701984 <kriollone@gmail.com>
2026-01-14 14:50:25 -0300

    feat: agregar pruebas unitarias básicas para la clase VersaORM

--- tests/unit/VersaORMMinimalTest.php
+++ tests/unit/VersaORMMinimalTest.php
@@ -1,1 +1,23 @@
+<?php
 
+declare(strict_types=1);
+
+namespace VersaORM\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class VersaORMMinimalTest extends TestCase
+{
+    public function testVersaORMClassExists(): void
+    {
+        static::assertTrue(class_exists('\\VersaORM\\VersaORM'));
+    }
+
+    public function testVersaORMCanBeInstantiated(): void
+    {
+        $orm = new \VersaORM\VersaORM(['driver' => 'sqlite', 'database' => ':memory:']);
+        static::assertInstanceOf('\\VersaORM\\VersaORM', $orm);
+    }
+}
